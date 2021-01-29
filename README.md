8b2ab9435443e531efc46d052353e34d39a8b750
AbnerBittencourt <abnerrocha00@gmail.com>
2021-01-29 16:08:52 -0300

    up project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz Bíblico Base
+
+Seja bem vindo ao projeto base do Quiz Bíblico!
+
+![Capa do Projeto](/_docs/capa.png)
+
+# Onde está o Layout base?
+- [Link](https://www.figma.com/file/cg1MIzSRRss8ggpypQbmdD/AluraQuiz?node-id=0%3A1)
+
---
+++ src/components/Footer/index.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+// src/components/Footer/index.js
+const FooterWrapper = styled.footer`
+  background-color: #F4CC8E;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  opacity: 0.6; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+      color: #000000;
+    }
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <p>
+        Orgulhosamente criado durante
+        {' '}
+        a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
